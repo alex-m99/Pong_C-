5f9c3a67f288809e7da4735d90ff5cd2692d8911
Manea Daniel Alexandru <alex.manea12@gmail.com>
2023-06-30 02:00:13 +0300

    Finish cBall class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-
+// enum este folosit pt a restrictiona valorile variabilei eDir (int) la doar una dintre cele enumerate in declaratie
 enum eDir
 { 
     STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6
@@ -18,11 +18,91 @@ class cBall
         {
             originalX = posX;
             originalY = posY;
+            x = posX;
+            y = posY;
+            direction = STOP;
+        }
+
+        void Reset()
+        {
+            x = originalX;
+            y = originalY;
+            direction = STOP;   
+        }
+
+        void changeDirection(eDir d)
+        {
+            direction = d;
+        }
+
+        void randomDirection()
+        {
+            direction = (eDir)((rand()%6) + 1 );
+        }
+
+        inline int getX()
+        {
+            return x;
+        }
+        
+        inline int getY()
+        {
+            return y;
+        }
+
+        inline int getDirection()
+        {
+            return direction;
+        }
+
+        void Move()
+        {
+            switch (direction)
+            {
+                case STOP:
+                    break;
+                case LEFT:
+                    x--;
+                    break;
+                case RIGHT:
+                    x++;
+                    break;
+                case UPLEFT:
+                    x--; y--;
+                    break;
+                case DOWNLEFT:
+                    x--; y++;
+                    break;
+                case UPRIGHT:
+                    x++; y--;
+                    break;
+                case DOWNRIGHT:
+                    x++; y++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        friend std::ostream & operator << (std::ostream & o, cBall c)
+        {
+            o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
+            return o;
         }
 };
 
 
 int main()
 {
+    cBall c(0, 0);
+    std::cout << c << std::endl;
+    c.randomDirection();
+    std::cout << c << std::endl;
+    c.Move();
+    std::cout << c << std::endl;
+    c. randomDirection();
+    c.Move();
+    std::cout << c << std::endl;
 
+    return 0;
 }
