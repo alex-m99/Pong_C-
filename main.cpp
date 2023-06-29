ad7a7622d6ce34bde9ed8ec6d8eb8477678168ea
Manea Daniel Alexandru <alex.manea12@gmail.com>
2023-06-30 00:53:28 +0300

    Add cBall class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-//main function
+
+enum eDir
+{ 
+    STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6
+
+};
+
+class cBall
+{
+    private:
+        int x, y;
+        int originalX, originalY;
+        eDir direction;
+    public:
+        cBall(int posX, int posY)
+        {
+            originalX = posX;
+            originalY = posY;
+        }
+};
+
 
 int main()
 {
